b6b07296c8c848933097608079a9aae87c99267f
diurno <diurno@pc-aula>
2019-03-11 09:15:31 +0100

    copia2

--- openarrays/src/openarrays/array.java
+++ openarrays/src/openarrays/array.java
@@ -13,6 +13,12 @@ public class array {
 		}
 	}
 	
+	private static void imprimirarray(int array[]) {
+		for (int i = 0; i < array.length; i++) {
+			System.out.println(array[i]);
+		}
+	}
+	
 	private static void sumaarray(int array[]) {
 		int suma = 0;
 		for (int i = 0; i < array.length; i++) {
