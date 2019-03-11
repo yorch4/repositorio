2f2dad95d43c517c202c73b0181ce15969525404
diurno <diurno@pc-aula>
2019-03-11 09:43:23 +0100

    rama1

--- openarrays/src/openarrays/array.java
+++ openarrays/src/openarrays/array.java
@@ -5,12 +5,17 @@ public class array {
 	public static void main(String[] args) {
 		int[] array = new int[10];
 		rellenaarray(array);
+		nuevometodo1();
 	}
 
 	private static void rellenaarray(int array[]) {
 		for (int i = 0; i < array.length; i++) {
 			array[i] = (int) (Math.round(Math.random() * 19) + 1);
 		}
+	}
+	
+	private static void nuevometodo1() {
+		System.out.println("Nuevo metodo");
 	}
 
 }
