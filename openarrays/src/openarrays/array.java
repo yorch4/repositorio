b8dfbdbe836c4d86eaa43547bcdcd61fb1a5bf12
diurno <diurno@pc-aula>
2019-03-11 09:33:06 +0100

    cambio

--- openarrays/src/openarrays/array.java
+++ openarrays/src/openarrays/array.java
@@ -6,7 +6,7 @@ public class array {
 		int[] array = new int[8];
 		rellenaarray(array);
 	}
-
+//CAMBIO2
 	private static void rellenaarray(int array[]) {
 		for (int i = 0; i < array.length; i++) {
 			array[i] = (int) (Math.round(Math.random() * 19) + 1);
