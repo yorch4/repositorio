6aa3f9a6df65f12cf4aaa8cd4a98673578d1d014
diurno <diurno@pc-aula>
2019-03-11 09:41:30 +0100

    nuevometodo2

--- openarrays/src/openarrays/array.java
+++ openarrays/src/openarrays/array.java
@@ -5,12 +5,17 @@ public class array {
 	public static void main(String[] args) {
 		int[] array = new int[8];
 		rellenaarray(array);
+		nuevometodo2();
 	}
 //CAMBIO2
 	private static void rellenaarray(int array[]) {
 		for (int i = 0; i < array.length; i++) {
 			array[i] = (int) (Math.round(Math.random() * 19) + 1);
 		}
+	}
+	
+	private static void nuevometodo2() {
+		System.out.println("Metodo 2");
 	}
 
 }
